6ab5a7c413f66198ba6add3ac4f37860f2950a6e
rgasali <96292535+rgasali@users.noreply.github.com>
2022-05-26 16:09:10 -0300

    invertirSec-Rec invertir arreglos secuenciales e invertir arreglos con recursividad

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-let arreglo1: number[] = new Array(1, 3, 9, 4, 5, 8);
-let arregloInvertido: number[] = new Array();
-let auxiliar: number;
+let arreglo1: number[] = new Array(1, 3, 9, 4, 5, 98);
+let arregloInvertidoSec: number[] = new Array();
+let arregloInvertidoRec: number[] = new Array();
+let index: number = 0;
 
 function invertirArregloSecuencial(
   arreglo: Array<number>,
@@ -11,5 +12,17 @@ function invertirArregloSecuencial(
   }
 }
 
-invertirArregloSecuencial(arreglo1, arregloInvertido);
-console.log(arregloInvertido);
+function invertirArregloRec(arreglo: Array<number>, index: number) {
+  if (index === arreglo.length - 1) {
+    arregloInvertidoRec.push(arreglo[index]);
+  } else {
+    invertirArregloRec(arreglo, index + 1);
+    arregloInvertidoRec.push(arreglo[index]);
+  }
+}
+
+invertirArregloSecuencial(arreglo1, arregloInvertidoSec);
+invertirArregloRec(arreglo1, index);
+
+console.log(arregloInvertidoSec);
+console.log(arregloInvertidoRec);
